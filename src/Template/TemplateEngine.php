8f5ebb8d31a2506fda64f9d3f398806ebe9955cb
Radenz <13520166@std.stei.itb.ac.id>
2022-10-22 15:29:08 +0700

    feat: implement list template binding

--- src/Template/TemplateEngine.php
+++ src/Template/TemplateEngine.php
@@ -8,11 +8,19 @@ class TemplateEngine
     private array $vars;
     private bool $bound;
 
+    // Flags
+    private bool $multiple;
+
+    // Intermediates
+    private array $replacements;
+
     function __construct($template)
     {
         $this->template = $template;
         $this->vars = [];
         $this->bound = false;
+        $this->multiple = false;
+        $this->replacements = [];
     }
 
     function render($vars = []): void
@@ -20,12 +28,34 @@ class TemplateEngine
         if (!$this->bound) {
             $this->vars = $vars;
         }
-        echo preg_replace_callback("/\{\{\s+(\w*)\s+}}/", array($this, "_getReplacement"), $this->template);
+        echo $this->_renderAsStringSingle();
     }
 
     function _renderAsString(): array|string|null
     {
+        if ($this->multiple) {
+            return $this->_renderAsStringMultiple();
+        }
+
+        return $this->_renderAsStringSingle();
+    }
+
+    function _renderAsStringSingle(): array|string|null
+    {
+        $this->replacements = $this->vars;
         return preg_replace_callback("/\{\{\s+(\w*)\s+}}/", array($this, "_getReplacement"), $this->template);
+    }
+
+    function _renderAsStringMultiple(): string
+    {
+        $components = [];
+
+        foreach ($this->vars as $vars) {
+            $this->replacements = $vars;
+            $components[] = preg_replace_callback("/\{\{\s+(\w*)\s+}}/", array($this, "_getReplacement"), $this->template);
+        }
+
+        return join("\n", $components);
     }
 
     function bind($vars): static
@@ -35,12 +65,18 @@ class TemplateEngine
         return $this;
     }
 
+    function bindEach($arr): static
+    {
+        $this->multiple = true;
+        return $this->bind($arr);
+    }
+
     function _getReplacement($match) {
 
         $name = $match[1];
 
-        if (array_key_exists($name, $this->vars)) {
-            $replacement = $this->vars[$name];
+        if (array_key_exists($name, $this->replacements)) {
+            $replacement = $this->replacements[$name];
             if ($replacement instanceof TemplateEngine) {
                 return $replacement->_renderAsString();
             }
