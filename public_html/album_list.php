d788d5026ed039c71b5e59574d544aea3b7b1f08
louisyeee <13520063@std.stei.itb.ac.id>
2022-10-26 11:20:48 +0700

    feat: make album list front end

--- public_html/album_list.php
+++ public_html/album_list.php
@@ -7,7 +7,7 @@ $albumList = $STORE->getAllAlbum();
 
 function make_table ($albumList) {
     $tbl_array = [];
-    $tbl_array[] = "<table>";
+    // $tbl_array[] = "<table>";
     foreach ($albumList as $album){
         $title = $album->getTitle();
         $artist = $album->getArtist();
@@ -17,18 +17,27 @@ function make_table ($albumList) {
         $path = "/assets/";
         $image_path = $path . $image_path;
         $genre = $album->getGenre();
-        $tbl_array[] = "<tr>";
-        $tbl_array[] = "<td rowspan=\"3\"><img src=\"$image_path\" width=\"300\" ></td>";
-        $tbl_array[] = "<td>$title</td>";
-        $tbl_array[] = "</tr>";
-        $tbl_array[] = "<tr>";
-        $tbl_array[] = "<td>$artist</td>";
-        $tbl_array[] = "</tr>";
-        $tbl_array[] = "<tr>";
-        $tbl_array[] = "<td>$tahun • $genre $image_path</td>";
-        $tbl_array[] = "</tr>";
+        // $tbl_array[] = "<tr>";
+        // $tbl_array[] = "<td rowspan=\"3\"><img src=\"$image_path\" width=\"300\" ></td>";
+        // $tbl_array[] = "<td>$title</td>";
+        // $tbl_array[] = "</tr>";
+        // $tbl_array[] = "<tr>";
+        // $tbl_array[] = "<td>$artist</td>";
+        // $tbl_array[] = "</tr>";
+        // $tbl_array[] = "<tr>";
+        // $tbl_array[] = "<td>$tahun • $genre $image_path</td>";
+        // $tbl_array[] = "</tr>";
+        $tbl_array[]= "<div id=\"albumList_hero\">";
+        $tbl_array[]= "<img class=\"cover\" src=\"$image_path\" alt=\"Kosong\">";
+        $tbl_array[]= "<div class=\"details\">";
+        $tbl_array[]= "<h2 class=\"item\">Album</h2>";
+        $tbl_array[]= "<h1 class=\"title\">$title</h1>";
+        $tbl_array[]= "<span class=\"details-meta\">$artist</span>";
+        $tbl_array[]= "<span class=\"details-meta\">$tahun • $genre</span>";
+        $tbl_array[]= "</div>";
+        $tbl_array[]= "</div>";
     }
-    $tbl_array[] = "</table>";
+    // $tbl_array[] = "</table>";
     
     return implode('', $tbl_array);
 
