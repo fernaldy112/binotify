624c3d64152a95d0b4bad865d7686d9c686d489a
louisyeee <13520063@std.stei.itb.ac.id>
2022-10-26 14:59:45 +0700

    feat: add show menu for admin

--- public_html/album_list.php
+++ public_html/album_list.php
@@ -17,21 +17,25 @@ function make_table ($albumList) {
         $path = "/assets/";
         $image_path = $path . $image_path;
         $genre = $album->getGenre();
-        // $tbl_array[] = "<tr>";
-        // $tbl_array[] = "<td rowspan=\"3\"><img src=\"$image_path\" width=\"300\" ></td>";
-        // $tbl_array[] = "<td>$title</td>";
-        // $tbl_array[] = "</tr>";
-        // $tbl_array[] = "<tr>";
-        // $tbl_array[] = "<td>$artist</td>";
-        // $tbl_array[] = "</tr>";
-        // $tbl_array[] = "<tr>";
-        // $tbl_array[] = "<td>$tahun • $genre $image_path</td>";
-        // $tbl_array[] = "</tr>";
         $tbl_array[]= "<div id=\"albumList_hero\">";
         $tbl_array[]= "<img class=\"cover\" src=\"$image_path\" alt=\"Kosong\">";
         $tbl_array[]= "<div class=\"details\">";
         $tbl_array[]= "<h2 class=\"item\">Album</h2>";
         $tbl_array[]= "<h1 class=\"title\">$title</h1>";
+        
+        // $tbl_array[]= "<div class=\"dropdown\">";
+            
+        // $tbl_array[]= "<ul class=\"dropbtn icons btn-right showLeft\" onclick=\"showDropdown()\">";
+        // $tbl_array[]= "<li></li>";
+        // $tbl_array[]= "<li></li>";
+        // $tbl_array[]= "<li></li>";
+        // $tbl_array[]= "</ul>";
+            
+        // $tbl_array[]= "<div id=\"myDropdown\" class=\"dropdown-content\">";
+        // $tbl_array[]= "<a href=\"#home\">Open</a>";
+        // $tbl_array[]= "<a href=\"#about\">Edit</a>";
+        // $tbl_array[]= "</div>";
+        // $tbl_array[]= "</div>";
         $tbl_array[]= "<span class=\"details-meta\">$artist</span>";
         $tbl_array[]= "<span class=\"details-meta\">$tahun • $genre</span>";
         $tbl_array[]= "</div>";
