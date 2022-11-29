d66caefd488bf4c687f3cf2ac3dc0331fc7c09e7
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2022-11-29 21:55:29 +0700

    feat: simple artistList page

--- public_html/premiumsinger.php
+++ public_html/premiumsinger.php
@@ -5,39 +5,45 @@ require_once(__DIR__."/../src/Store/DataStore.php");
 require_once(__DIR__."/../src/components/header.php");
 require_once(__DIR__."/../src/components/navbar.php");
 
-$xml = file_get_contents("http://rest/artistList");
-echo($xml);
+// $data = file_get_contents("http://rest/artistList");
+$data = '[{"user_id":1,"name":"exampleName"}]';
+$artistList = json_decode($data);
 
 if (!isset($STORE) || !isset($NAVBAR) || !isset($HEADER)) {
     http_response_code(500);
     return;
 }
 
+function makeTable($artistList){
+    $tbl_array = [];
+    $tbl_array[] = "<table class=\"styled-table\">";
+    $tbl_array[] = "<tr><th>Artist ID</th><th>Artist Name</th></tr>";
+    foreach ($artistList as $artist){
+        $tbl_array[] = "<tr>";
+        $tbl_array[] = "<td>$artist->user_id</td>";
+        $tbl_array[] = "<td>$artist->name</td>";
+        $tbl_array[] = "</tr>";
+    }
+    $tbl_array[] = "</table>";
+    
+    return implode('', $tbl_array);
+}
+
 $header = html("components/shared/header.html");
-$result = template("components/search/result.html")->bind(["query" => $query]);
-$noResult = template("components/search/no-result.html")->bind(["query" => $query]);
-$main = template("components/search/main.html")->bind([
-    "search_result" => $hasResult ? $result : $noResult,
-    "header" => $HEADER
+$main = template("components/premiumsinger/main.html")->bind([
+    "header" => $HEADER,
+    "user_list" => makeTable($artistList)
 ]);
 
-css("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap");
+css("css/user_list.css");
 css("css/styles.css");
 css("css/shared.css");
-css("css/search.css");
 css("https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200");
 css("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200");
-js("js/table.js");
-js("js/search.js");
-js("js/util.js");
-js("js/searchbar.js");
 js("js/auth.js");
 
 template("components/premiumsinger.html")->bind([
-    "query" => $query,
+    "title" => "Premium Artist List",
     "navbar" => $NAVBAR,
-    "main" => $main,
-    "json_result" => json_encode($res),
-    "genres" => json_encode($genres),
-    "page" => $page
+    "main" => $main
 ])->render();
