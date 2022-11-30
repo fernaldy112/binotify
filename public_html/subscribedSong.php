3e3faa0d57088fc669feefd568d084bae6111497
Radenz <radenrifqirahman@gmail.com>
2022-11-30 23:19:23 +0700

    chore: rename subscribed to subscribedSong

--- public_html/subscribedSong.php
+++ public_html/subscribedSong.php
@@ -39,15 +39,17 @@ function make_tabel($subscribedSongList){
     foreach ($subscribedSongList as $song){
         $tbl_array[] = "<tr>";
         $judul = $song->judul;
-        // $playBar = template("components/listen/play-bar.html")->bind([
-        //     "cover" => $song->getImagePath(),
-        //     "cover_alt" => $song->getTitle(),
-        //     "title" => $song->getTitle(),
-        //     "artist" => $song->getArtist(),
-        //     "duration" => $song->getDurationString()
-        // ]);
         $tbl_array[] = "<td>$judul</td>";
-        // $tbl_array[] = "<td>$playBar</td>";
+        $tbl_array[] = "<td>";
+        $tbl_array[] = "<div class=\"bar\">";
+        $tbl_array[] = "<button class=\"play-button\">";
+        $tbl_array[] = "<span class=\"material-symbols-rounded icon\">";
+        $tbl_array[] = "play_arrow";
+        $tbl_array[] = "</span>";
+        $tbl_array[] = "</button>";
+        $tbl_array[] = "</div>";
+        $tbl_array[] = "<audio hidden controls class=\"audio-playback\" src=\"$audioPath\" preload=\"auto\"></audio>";
+        $tbl_array[] = "<td>";
         $tbl_array[] = "</tr>";
     }
     $tbl_array[] = "</table>";
@@ -61,15 +63,25 @@ $main = template("components/user_list/main.html")->bind([
     "user_list" => make_tabel($subscribedSongList)
 ]);
 
+$playBar = template("components/premium_song/play-bar.html")->bind([
+    "title" => $song->judul,
+]);
+
 css("css/user_list.css");
 css("css/styles.css");
+css("css/listen.css");
 css("css/shared.css");
+css("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 css("https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200");
 css("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200");
 js("js/auth.js");
+js("js/util.js");
+js("js/player.js");
+js("js/listen.js");
 
 template("components/premiumsingersong.html")->bind([
     "title" => "Premium Song",
     "navbar" => $NAVBAR,
-    "main" => $main
+    "main" => $main,
+    "playBar" => $playBar,
 ])->render();
